9bef63b98d1b39103c08cb211c4d673c1c553f87
Raul Ferrando <rferrando@okode.com>
2024-07-24 10:00:11 +0200

    add java lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -51,7 +51,6 @@ return {
                     })
                     vim.g.zig_fmt_parse_errors = 0
                     vim.g.zig_fmt_autosave = 0
-
                 end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
@@ -66,7 +65,7 @@ return {
                             }
                         }
                     }
-                end,
+                end
             }
         })
 
